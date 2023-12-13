c1322932de58df9308566f428cef0e9fd8e45a2a
aaronr6 <aaronb.0103@gmail.com>
2023-12-13 12:56:03 +0100

    Uploadanje Vjezba 3

--- Vjezba 3/# Vjezba 3 - prepoznavanje lica.py
+++ Vjezba 3/# Vjezba 3 - prepoznavanje lica.py
@@ -1,1 +1,27 @@
 # Vjezba 3 - prepoznavanje lica
+
+# print(cv.__file__)
+# print(cv.data.haarcascades)
+
+import cv2 as cv
+
+original_image = cv.imread('moje_lice.jpg')
+
+grayscale_image = cv.cvtColor(original_image, cv.COLOR_BGR2GRAY)
+
+face_cascade = cv.CascadeClassifier('haarcascade_frontalface_alt.xml')
+
+detected_faces = face_cascade.detectMultiScale(grayscale_image)
+
+for (column, row, width, height) in detected_faces:
+    cv.rectangle(
+        original_image,
+        (column, row),
+        (column + width, row + height),
+        (0, 255, 0),
+        2
+    )
+
+cv.imshow('Image', original_image)
+cv.waitKey(0)
+cv.destroyAllWindows()
