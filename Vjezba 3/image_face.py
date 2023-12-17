a36092390638e6160296cbaacd7d9e32764dc96d
aaronr6 <aaronb.0103@gmail.com>
2023-12-18 00:32:34 +0100

    za sad dosta

--- Vjezba 3/image_face.py
+++ Vjezba 3/image_face.py
@@ -4,45 +4,57 @@ from round_rectangle import *
 from new_line import *
 from deepface import DeepFace
 
-img = cv.imread('aaron_2.jpg')
+def run_image():
 
-img_temp = img.copy()
-unique, counts = np.unique(img_temp.reshape(-1, 3), axis=0, return_counts=True)
-dominant_color = tuple(unique[np.argmax(counts)])
+    img = cv.imread('aaron_2.jpg')
 
-analysis = DeepFace.analyze(img, actions = ['age', 'gender', 'race', 'emotion'])
-analysis = analysis[0]
-# print(analysis)
-# print(analysis["age"], "years old", analysis["dominant_race"], " ", analysis["dominant_emotion"], " ", analysis["dominant_gender"])
+    img_temp = img.copy()
+    unique, counts = np.unique(img_temp.reshape(-1, 3), axis=0, return_counts=True)
+    dominant_color = list(unique[np.argmax(counts)])
+    print(dominant_color)
+    dominant_color2 = []
+    for i in dominant_color:
+        dominant_color2.append(int(i))
 
-parameters = analysis["region"]
-# print(parameters)
 
-draw_border(img, (parameters["x"],parameters["y"]), (parameters["x"]+parameters["w"], parameters["y"]+parameters["h"]), (226, 204, 120), 5, 10, 20)
+    analysis = DeepFace.analyze(img, actions = ['age', 'gender', 'race', 'emotion'])
+    analysis = analysis[0]
+    # print(analysis)
+    # print(analysis["age"], "years old", analysis["dominant_race"], " ", analysis["dominant_emotion"], " ", analysis["dominant_gender"])
 
-text = f'''AGE: {analysis["age"]}
-RACE: {analysis["dominant_race"]}
-EMOTION: {analysis["dominant_emotion"]}
-GENDER: {analysis["dominant_gender"]}'''
+    parameters = analysis["region"]
+    # print(parameters)
 
-font = cv.FONT_HERSHEY_SIMPLEX 
-position = (parameters["x"]+parameters["w"]+10, parameters["y"]) 
-font_scale = 1
-font_color = (102, 80, 0)
-line_thickness = 2
+    draw_border(img, (parameters["x"],parameters["y"]), (parameters["x"]+parameters["w"], parameters["y"]+parameters["h"]), (226, 204, 120), 5, 10, 20)
 
-text_size, baseline = cv.getTextSize(text, font, font_scale, line_thickness)
-text_width = text_size[0]
-text_width = int(text_width)
+    text = f'''AGE: {analysis["age"]}
+    RACE: {analysis["dominant_race"]}
+    EMOTION: {analysis["dominant_emotion"]}
+    GENDER: {analysis["dominant_gender"]}'''
 
-height, width, channels = img.shape
-width = int(width)
+    check_text1 = f"EMOTION: {analysis['dominant_emotion']}"
+    check_text2 = f"RACE: {analysis['dominant_race']}"
 
-print(type(dominant_color))
+    font = cv.FONT_HERSHEY_SIMPLEX 
+    position = (parameters["x"]+parameters["w"]+10, parameters["y"]) 
+    font_scale = 1
+    font_color = (102, 80, 0)
+    line_thickness = 2
 
-if width + text_width + 10 > width:
-    img = cv.copyMakeBorder(img, 0, 0, 0, width, cv.BORDER_CONSTANT, value = dominant_color)
+    text_size1, baseline1 = cv.getTextSize(check_text1, font, font_scale, line_thickness)
+    text_width1 = text_size1[0]
+    text_width1 = int(text_width1)
 
-add_text_to_image(img, text, position, font_scale, line_thickness, font, font_color)
+    text_size2, baseline2 = cv.getTextSize(check_text2, font, font_scale, line_thickness)
+    text_width2 = text_size2[0]
+    text_width2 = int(text_width2)
 
-cv.imwrite('detected.png', img)
+    height, width, channels = img.shape
+    width = int(width)
+
+    if parameters["x"]+parameters["w"] + text_width1 + 10 > width or parameters["x"]+parameters["w"] + text_width2 + 10 > width:
+        img = cv.copyMakeBorder(img, 0, 0, 0, width, cv.BORDER_CONSTANT, value = dominant_color2)
+
+    add_text_to_image(img, text, position, font_scale, line_thickness, font, font_color)
+
+    cv.imwrite('detected.png', img)
--- Vjezba 3/video_face.py
+++ Vjezba 3/video_face.py
@@ -4,37 +4,39 @@ from round_rectangle import *
 from new_line import *
 from deepface import DeepFace
 
-face_cascade = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_frontalface_default.xml')
-cap = cv.VideoCapture(0)
+def run_video():
 
-while True:
-    ret, frame = cap.read()
+    face_cascade = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    cap = cv.VideoCapture(0)
 
-    faces = face_cascade.detectMultiScale(frame, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-    
-    for (x, y, w, h) in faces:
+    while True:
+        ret, frame = cap.read()
 
-        face = frame[y:y + h, x:x + w]
+        faces = face_cascade.detectMultiScale(frame, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+        
+        for (x, y, w, h) in faces:
 
-        result = DeepFace.analyze(face, actions=['age', 'gender', 'emotion', 'race'], enforce_detection=False)
-        result = result[0]
+            face = frame[y:y + h, x:x + w]
 
-        text_age = f"AGE: {result['age']}"
-        text_gender = f"GENDER: {result['dominant_gender']}"
-        text_emotion = f"EMOTION: {result['dominant_emotion']}"
-        text_race = f"RACE: {result['dominant_race']}"
+            result = DeepFace.analyze(face, actions=['age', 'gender', 'emotion', 'race'], enforce_detection=False)
+            result = result[0]
 
-        draw_border(frame, (x, y), (x + w, y + h), (226, 204, 120), 5, 10, 20)
+            text_age = f"AGE: {result['age']}"
+            text_gender = f"GENDER: {result['dominant_gender']}"
+            text_emotion = f"EMOTION: {result['dominant_emotion']}"
+            text_race = f"RACE: {result['dominant_race']}"
 
-        cv.putText(frame, text_age, (x + w + 10, y + 20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-        cv.putText(frame, text_gender, (x + w + 10, y + 40), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-        cv.putText(frame, text_emotion, (x + w + 10, y + 60), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-        cv.putText(frame, text_race, (x + w + 10, y + 80), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+            draw_border(frame, (x, y), (x + w, y + h), (226, 204, 120), 5, 10, 20)
 
-    cv.imshow('Face Recognition', frame)
+            cv.putText(frame, text_age, (x + w + 10, y + 20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+            cv.putText(frame, text_gender, (x + w + 10, y + 40), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+            cv.putText(frame, text_emotion, (x + w + 10, y + 60), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+            cv.putText(frame, text_race, (x + w + 10, y + 80), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 
-    if cv.waitKey(1) & 0xFF == ord('q'):
-        break
+        cv.imshow('Face Recognition', frame)
 
-cap.release()
-cv.destroyAllWindows()
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv.destroyAllWindows()
